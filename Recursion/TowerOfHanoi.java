cea2e008f91687a5ddf2a655bc46950ef2818841
Punith <144771860+Puneeetth@users.noreply.github.com>
2026-04-06 07:33:02 +0000

    06-04(towerOfHanoi)

---
+++ Recursion/TowerOfHanoi.java
@@ -0,0 +1,15 @@
+class TowerOfHanoi {
+    public static void towerOfHanoi(int n,char frompeg,char topeg,char auxpeg){
+        if(n == 1) {
+        System.out.println("Move disk 1 from peg " + frompeg + " to peg " + topeg);
+        return;
+        }
+        
+        towerOfHanoi(n - 1,frompeg,auxpeg,topeg);
+        System.out.println("Move disk " + n + " from peg " + frompeg + " to peg" + topeg);
+        towerOfHanoi(n - 1,auxpeg,topeg,frompeg);
+    }
+    public static void main(String[] args) {
+         towerOfHanoi(3,'A','B','C');
+    }
+}
