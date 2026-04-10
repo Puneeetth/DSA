270e586b52f7e091218500fc6c471ac64ee2430d
Punith <punithreddy417@gmail.com>
2026-04-10 23:20:59 +0530

    merge

--- Recursion/TowerOfHanoi.java
+++ Recursion/TowerOfHanoi.java
@@ -1,13 +1,16 @@
+package Recursion;
 class TowerOfHanoi {
     public static void towerOfHanoi(int n,char frompeg,char topeg,char auxpeg){
+        if(n == 0) return;
         if(n == 1) {
         System.out.println("Move disk 1 from peg " + frompeg + " to peg " + topeg);
         return;
         }
         
         towerOfHanoi(n - 1,frompeg,auxpeg,topeg);
-        System.out.println("Move disk " + n + " from peg " + frompeg + " to peg " + topeg);
+        System.out.println("Move disk " + n + " from peg " + frompeg + " to peg" + topeg);
         towerOfHanoi(n - 1,auxpeg,topeg,frompeg);
+
     }
     public static void main(String[] args) {
          towerOfHanoi(3,'A','B','C');
